70b6c5202a449157e26eae35c4be1989ac3d59ba
oh <outputor777@gmail.com>
2022-08-06 02:01:21 +0900

    mockup

---
+++ src/components/Domains/Domains.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Domains = () => {
+  return (
+    <Container>
+      <HeaderTitle>모두의 투두 리스트</HeaderTitle>
+      <ContentsBox>
+        <Content>
+          <Text>할 일 기록하기</Text>
+        </Content>
+        <Content>
+          <Text>TODO LIST</Text>
+        </Content>
+      </ContentsBox>
+    </Container>
+  );
+};
+
+export default Domains;
+
+const Container = styled.div``;
+const HeaderTitle = styled.h1`
+  color: #6aaecd;
+  text-align: center;
+  font-size: 1.8rem;
+`;
+
+const ContentsBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+`;
+const Content = styled.div`
+  width: 500px;
+  height: 80px;
+  margin: 1rem 0 1.5rem 0;
+  background-color: #6aaecd;
+  padding: 0.5rem 0 0 0;
+  border-radius: 10px;
+  box-sizing: border-box;
+  text-align: center;
+  font-size: 1.2rem;
+  cursor: pointer;
+  -webkit-box-shadow: 0px 7px 9px -1px rgba(0, 0, 0, 0.65);
+  box-shadow: 0px 7px 9px -1px rgba(0, 0, 0, 0.65);
+`;
+const Text = styled.p`
+  color: #f0efe9;
+`;
